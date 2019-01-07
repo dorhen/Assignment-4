91b953e37571f6d1cb8421e523178f72b1614d85
danbar06 <44751586+danbar06@users.noreply.github.com>
2019-01-07 18:14:16 +0200

    Update schedule.py

--- schedule.py
+++ schedule.py
@@ -24,41 +24,59 @@ def loop():
         for row in classrooms:
             if row[3]==1:
                 c.execute("""
-                SELECT course_name FROM courses WHERE id=row[2];
-                """)
-                print('(' + counter + ') ' + row[1] + ': ' + c.fetchone() + " is done")
+                SELECT course_name FROM courses WHERE id=?;
+                """, [row[2]])
+                arr = c.fetchone()
+                print('(' + str(counter) + ') ' + row[1] + ': ' + arr[0] + " is done")
                 conn.execute("""
-                DELETE FROM courses WHERE id=row[2];
-                """)
+                DELETE FROM courses WHERE id=?;
+                """, [row[2]])
+                conn.execute("""
+                UPDATE classrooms
+                SET current_course_time_left=0
+                WHERE current_course_id=?
+                """, [row[2]])
                 numOfCourses = numOfCourses-1
-                row[3]=0
+                row = [row[0],row[1],row[2],0]
             if row[3] == 0:
                 c.execute("""
                 SELECT * FROM courses
-                WHERE class_id=row[0];
-                """)
+                WHERE class_id=?;
+                """,[row[0]])
                 toStart = c.fetchone()
-                if not len(toStart)==0:
-                    conn.executescript("""
+                if not toStart is None:
+                    conn.execute("""
                     UPDATE students
-                    SET number_of_students = number_of_students-toStart[3]
-                    WHERE students=toStart[2];
-                    
+                    SET count = count - ?
+                    WHERE grade=?;
+                    """,[toStart[3],toStart[2]])
+
+                    conn.execute("""
                     UPDATE classrooms 
-                    SET current_course_id = toStart[0] current_course_time_left=toStart[5]
-                    WHERE id=row[0];
-                    """)
-                    print('('+counter+') '+row[1]+': '+toStart[1]+" is scheduled to start")
+                    SET current_course_id = ?, current_course_time_left=?
+                    WHERE id=?;
+                    """,[toStart[0],toStart[5] ,row[0]])
+                    print('('+str(counter)+') '+row[1]+': '+toStart[1]+" is scheduled to start")
             else:
                 c.execute("""
-                SELECT course_name FROM courses WHERE id=row[2];
-                """)
-                print('(' + counter + ') ' + row[1] + ": occupied by" + c.fetchone())
+                SELECT course_name FROM courses WHERE id=?;
+                """,[row[2]])
+                print('(' + str(counter) + ') ' + row[1] + ": occupied by " + c.fetchone()[0])
                 conn.execute("""
                 UPDATE courses
-                SET course_length = course_length-1
-                WHERE id=row[2];
-                """)
+                SET course_length = course_length-?
+                WHERE id=?;
+                """,[1,row[2]])
+                conn.execute("""
+                UPDATE classrooms
+                SET current_course_time_left = current_course_time_left-?
+                WHERE current_course_id=?;
+                """, [1, row[2]])
+            conn.commit()
+        c.execute("""
+        SELECT * FROM classrooms;
+        """)
+        classrooms = c.fetchall()
         counter = counter+1
     close_db(conn)
 
