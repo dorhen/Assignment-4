85514e3f5be78bc9723f42592fa660289cbea214
dorhen <44751515+dorhen@users.noreply.github.com>
2019-01-08 00:18:44 +0200

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -3,52 +3,55 @@ import optparse
 import os
 import schedule
 
-if __name__ == '__main__':
-    fileExist =os.path.isfile('classes.db')
-    if not fileExist:
+
+def main():
+    file_exist = os.path.isfile('classes.db')
+    if not file_exist:
         conn = sqlite3.connect("classes.db")
         conn.executescript("""
-            CREATE TABLE courses (
-            id INTEGER PRIMARY KEY,
-            course_name TEXT NOT NULL,
-            student TEXT NOT NULL,
-            number_of_students INTEGER NOT NULL,
-            class_id INTEGER REFERENCES  classrooms(id),
-            course_length INTEGER NOT NULL
-            );
-            
-            CREATE TABLE students (
-            grade TEXT PRIMARY KEY,
-            count INTEGER NOT NULL
-            );
-            
-            CREATE TABLE classrooms (
-            id INTEGER PRIMARY KEY,
-            location TEXT NOT NULL,
-            current_course_id INTEGER NOT NULL,
-            current_course_time_left INTEGER NOT NULL 
-            );
-        """)
+                CREATE TABLE courses (
+                id INTEGER PRIMARY KEY,
+                course_name TEXT NOT NULL,
+                student TEXT NOT NULL,
+                number_of_students INTEGER NOT NULL,
+                class_id INTEGER REFERENCES  classrooms(id),
+                course_length INTEGER NOT NULL
+                );
+
+                CREATE TABLE students (
+                grade TEXT PRIMARY KEY,
+                count INTEGER NOT NULL
+                );
+
+                CREATE TABLE classrooms (
+                id INTEGER PRIMARY KEY,
+                location TEXT NOT NULL,
+                current_course_id INTEGER NOT NULL,
+                current_course_time_left INTEGER NOT NULL 
+                );
+            """)
         parser = optparse.OptionParser()
         args = parser.parse_args()
-        file_object = open(args[1][0])
+        file_object = open(args[1][0], "r")
         for line in file_object:
             line = line.rstrip()
-            split=line.split(', ')
-            if split[0]=='C':
+            split = line.split(', ')
+            if split[0] == 'C':
                 conn.execute("""
                 INSERT INTO courses(id,course_name,student,number_of_students,class_id,course_length) VALUES (?,?,?,?,?,?);
                 """, split[1:])
-            if split[0]=='S':
+            if split[0] == 'S':
                 conn.execute("""
                 INSERT INTO students(grade,count) VALUES (?,?);
                 """, split[1:])
-            if split[0]=='R':
-                ans=split[1:]+[-1,0]
+            if split[0] == 'R':
+                ans = split[1:] + ["-1", "0"]
                 conn.execute("""
-                INSERT INTO classrooms(id,location,current_course_id,current_course_time_left) VALUES (?,?,?,?);
-                """, ans)
+                    INSERT INTO classrooms(id,location,current_course_id,current_course_time_left) VALUES (?,?,?,?);
+                    """, ans)
         conn.commit()
         schedule.loop()
 
 
+if __name__ == '__main__':
+    main()
