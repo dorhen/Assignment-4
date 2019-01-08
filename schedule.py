8ad8291e36c0078ab47ec6805af9cd113633d467
danbar06 <44751586+danbar06@users.noreply.github.com>
2019-01-08 09:09:24 +0200

    Update schedule.py

--- schedule.py
+++ schedule.py
@@ -9,16 +9,19 @@ def close_db(conn):
 
 def print_tables(table1, table2, table3):
     print("courses")
-    print(table1[0])
+    for line in table1:
+        print(line)
     print("classrooms")
-    print(table2[0])
+    for line in table2:
+        print(line)
     print("students")
-    print(table3[0])
+    for line in table3:
+        print(line)
 
 
-def loop():
+def main():
     counter = 0
-    conn = sqlite3.connect("classes.db")
+    conn = sqlite3.connect("schedule.db")
     c = conn.cursor()
     c.execute("""
     SELECT * FROM students;
@@ -33,63 +36,74 @@ def loop():
     """)
     classrooms = c.fetchall()
     num_of_courses = len(courses)
-    while num_of_courses > 0 and os.path.isfile('classes.db'):
-        for row in classrooms:
-            if row[3] == 1:
-                c.execute("""
-                SELECT course_name FROM courses WHERE id=?;
-                """, [row[2]])
-                arr = c.fetchone()
-                print('(' + str(counter) + ') ' + row[1] + ': ' + arr[0] + " is done")
-                conn.execute("""
-                DELETE FROM courses WHERE id=?;
-                """, [row[2]])
-                conn.execute("""
-                UPDATE classrooms
-                SET current_course_time_left=0
-                WHERE current_course_id=?
-                """, [row[2]])
-                num_of_courses = num_of_courses - 1
-                row = [row[0], row[1], row[2], 0]
-            if row[3] == 0:
-                c.execute("""
-                SELECT * FROM courses
-                WHERE class_id=?;
-                """, [row[0]])
-                to_start = c.fetchone()
-                if to_start is not None:
+    if num_of_courses ==0:
+        print(courses,classrooms,students)
+        close_db(conn)
+    else:
+        while num_of_courses > 0 and os.path.isfile('schedule.db'):
+            for row in classrooms:
+                if row[3] == 1:
+                    c.execute("""
+                    SELECT course_name FROM courses WHERE id=?;
+                    """, [row[2]])
+                    arr = c.fetchone()
+                    print('(' + str(counter) + ') ' + row[1] + ': ' + arr[0] + " is done")
                     conn.execute("""
-                    UPDATE students
-                    SET count = count - ?
-                    WHERE grade=?;
-                    """, [to_start[3], to_start[2]])
+                    DELETE FROM courses WHERE id=?;
+                    """, [row[2]])
+                    conn.execute("""
+                    UPDATE classrooms
+                    SET current_course_time_left=0, current_course_id =0
+                    WHERE current_course_id=?
+                    """, [row[2]])
+                    num_of_courses = num_of_courses - 1
+                    row = [row[0], row[1], row[2], 0]
+                if row[3] == 0:
+                    c.execute("""
+                    SELECT * FROM courses
+                    WHERE class_id=?;
+                    """, [row[0]])
+                    to_start = c.fetchone()
+                    if to_start is not None:
+                        conn.execute("""
+                        UPDATE students
+                        SET count = count - ?
+                        WHERE grade=?;
+                        """, [to_start[3], to_start[2]])
 
+                        conn.execute("""
+                        UPDATE classrooms 
+                        SET current_course_id = ?, current_course_time_left=?
+                        WHERE id=?;
+                        """, [to_start[0], to_start[5], row[0]])
+                        print('(' + str(counter) + ') ' + row[1] + ': ' + to_start[1] + " is schedule to start")
+                else:
+                    c.execute("""
+                    SELECT course_name FROM courses WHERE id=?;
+                    """, [row[2]])
+                    print('(' + str(counter) + ') ' + row[1] + ": occupied by " + c.fetchone()[0])
                     conn.execute("""
-                    UPDATE classrooms 
-                    SET current_course_id = ?, current_course_time_left=?
-                    WHERE id=?;
-                    """, [to_start[0], to_start[5], row[0]])
-                    print('(' + str(counter) + ') ' + row[1] + ': ' + to_start[1] + " is scheduled to start")
-            else:
-                c.execute("""
-                SELECT course_name FROM courses WHERE id=?;
-                """, [row[2]])
-                print('(' + str(counter) + ') ' + row[1] + ": occupied by " + c.fetchone()[0])
-                conn.execute("""
-                UPDATE courses
-                SET course_length = course_length-?
-                WHERE id=?;
-                """, [1, row[2]])
-                conn.execute("""
-                UPDATE classrooms
-                SET current_course_time_left = current_course_time_left-?
-                WHERE current_course_id=?;
-                """, [1, row[2]])
-            conn.commit()
-        c.execute("""
-        SELECT * FROM classrooms;
-        """)
-        classrooms = c.fetchall()
-        counter = counter + 1
-        print_tables(courses,classrooms,students)
-    close_db(conn)
+                    UPDATE classrooms
+                    SET current_course_time_left = current_course_time_left-?
+                    WHERE current_course_id=?;
+                    """, [1, row[2]])
+                conn.commit()
+            c.execute("""
+            SELECT * FROM students;
+            """)
+            students = c.fetchall()
+            c.execute("""
+            SELECT * FROM courses;
+            """)
+            courses = c.fetchall()
+            c.execute("""
+            SELECT * FROM classrooms;
+            """)
+            classrooms = c.fetchall()
+            counter = counter + 1
+            print_tables(courses,classrooms,students)
+        close_db(conn)
+
+
+if __name__ == '__main__':
+    main()
